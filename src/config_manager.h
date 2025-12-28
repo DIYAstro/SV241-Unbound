6da2fbf8fdaf9aa15287153f6975938795c8ffb8
DiMoGro1 <110323263+DiMoGro1@users.noreply.github.com>
2025-12-28 20:09:16 +0100

    Release 0.9.13

--- src/config_manager.h
+++ src/config_manager.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include "ArduinoJson.h"
 
-#define FIRMWARE_VERSION "0.9.9"
+#define FIRMWARE_VERSION "0.9.10"
 
 // Maximum number of supported dew heaters
 // Maximum number of supported dew heaters
