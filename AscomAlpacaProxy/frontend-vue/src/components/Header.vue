ea68692da34449cfac50014e3c895a9675a664ee
DiMoGro1 <110323263+DiMoGro1@users.noreply.github.com>
2026-02-11 16:51:01 +0100

    feat(gui): set Material Dark as default and eliminate theme flicker

--- AscomAlpacaProxy/frontend-vue/src/components/Header.vue
+++ AscomAlpacaProxy/frontend-vue/src/components/Header.vue
@@ -28,8 +28,8 @@ const handleThemeChange = (event) => {
                   </svg>
               </label>
               <select id="theme-select" v-model="currentTheme" @change="handleThemeChange" class="theme-select">
-                  <option :value="THEMES.DEFAULT">Deep Space</option>
                   <option :value="THEMES.DARK">Material Dark</option>
+                  <option :value="THEMES.DEFAULT">Deep Space</option>
                   <option :value="THEMES.RED">Night Vision</option>
               </select>
           </div>
